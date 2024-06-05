63e497d34151e9dd54aeab27ec5cc32c3bdc1dd4
Richard <richardjonhshm@gmail.com>
2024-06-05 10:52:05 -0300

    Enviar AppServidorPara o Servidor

--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,16 @@ Route::get('/', function () {
 Route::get("/configuracao",function(){
     return view('configuracao');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::get("/categoria",[CategoriaController::class,'index'])->middleware(['auth', 'verified'])->name('categoria.index');
 Route::get('/categoria/{slug}', [CategoriaController::class, 'show'])->name('categoria.show');
-
 Route::post("/categoria",[CategoriaController::class,'cadastrar'])->middleware(['auth', 'verified'])->name('categoria');
 Route::delete('/categorias/{id}', [CategoriaController::class, 'destroy'])->name('categorias.destroy');
-
 Route::post('/pdf/upload', [PdfController::class, 'upload'])->name('pdf.upload');
-
-
 Route::get("/pdf",[PdfController::class, 'index'])->middleware(['auth', 'verified'])->name('pdf');
-
 Route::get('/dashboard', [DashboardController::class,'index'])->middleware(['auth', 'verified'])->name('home');
-
 Route::get('/subscribe',[SubscribeController::class,'index'])
     ->name('subscribe')
     ->middleware(\Illuminate\Auth\Middleware\Authenticate::class);
-
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
